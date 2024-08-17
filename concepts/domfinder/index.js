5bf8f8b4bd84546b12908a0651c334dcb269f4be
mbeesetty <madhusudhanrao_beesetty@intuit.com>
2024-08-16 21:51:17 -0700

    dom finder

---
+++ concepts/domfinder/index.js
@@ -0,0 +1,12 @@
+const getPathFromChildToParent = (parent, child) => {
+  // enter your code here
+};
+
+const getValueFromPath = (parent, path) => {
+  // enter your code here
+};
+
+const findNodeValue = () => {
+  // enter your code here
+};
+document.getElementById("output").innerHTML = findNodeValue();
