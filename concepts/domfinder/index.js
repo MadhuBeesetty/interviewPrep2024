60f3fb9a7a45ef4812779c742d48533ca57eceaa
mbeesetty <madhusudhanrao_beesetty@intuit.com>
2024-08-17 17:30:55 -0700

    solution

--- concepts/domfinder/index.js
+++ concepts/domfinder/index.js
@@ -1,12 +1,31 @@
 const getPathFromChildToParent = (parent, child) => {
-  // enter your code here
+  let currentNode = child
+  const pathArray = []
+  while(currentNode !== parent) {
+    const parentElement = currentNode.parentElement
+    const childrenArray = Array.from(parentElement.children)
+    console.log(childrenArray, "this is children ARRAY");
+    pathArray.push(childrenArray.indexOf(currentNode))
+    console.log(pathArray, "this is pathArray");
+    currentNode = parentElement
+  }
+  return pathArray
 };
 
 const getValueFromPath = (parent, path) => {
-  // enter your code here
+    let currentNode = parent
+    while(path.length) {
+        currentNode = currentNode.children[path.pop()]
+    }
+    return currentNode.innerText
 };
 
 const findNodeValue = () => {
-  // enter your code here
+  const rootA = document.getElementById('rootA');
+  const rootB = document.getElementById("rootB");
+  const nodeA = document.getElementById("nodeA");
+  const path = getPathFromChildToParent(rootA, nodeA);
+  return getValueFromPath(rootB, path);
 };
+console.log(findNodeValue());
 document.getElementById("output").innerHTML = findNodeValue();
