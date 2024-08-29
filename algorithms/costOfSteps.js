83d66ea92fa078ce78e8dd4c929b79681b048812
mbeesetty <madhusudhan9kings@gmail.com>
2024-08-28 21:44:15 -0700

    cost of steps

--- algorithms/costOfSteps.js
+++ algorithms/costOfSteps.js
@@ -6,8 +6,19 @@
 // console.log(recfun([1, 10, 20, 15, 3, 6, 9, 8]));
 // console.log(recfun([1, 10, 21, 25, 24, 30, 33, 38]));
 
-function recfun(input) {
-    console.log('What the fuck is ' + JSON.stringify(input) + '? I don\'t know what this is.')
+const recfun = (input) => {
+  // [1, 10, 20, 15, 3, 6, 9, 8]
+  // [1, 10, 21, 25, 24, 30, 33, 38]
+  // ans[1,20,3,6,8]
+
+    if(input.length <= 2) return Math.min(input[1], input[0]);
+
+    const minCal = (input, i) => {
+        if(i === input.length) return Math.min(input[input.length - 1], input[input.length - 2]);
+        input[i] = Math.min(input[i - 1], input[i - 2]) + input[i];
+        return minCal(input, i + 1);
+    }
+    return minCal(input, 2);
 }
 
-recfun([1,2,3,4,5])
+console.log(recfun([1, 10, 20, 15, 3, 6, 9, 8]));
