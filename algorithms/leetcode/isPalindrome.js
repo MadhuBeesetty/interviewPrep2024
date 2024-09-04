fb6cc6de24090e133237b6556629b89b6eb4018d
MadhuBeesetty <madhusudhan9kings.com>
2024-09-04 16:11:19 -0700

    palindrome verification

---
+++ algorithms/leetcode/isPalindrome.js
@@ -0,0 +1,54 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+const isPalindrome = (s) => {
+
+if(s.length <= 1) return true;
+
+  function isLetter(char) {
+    const charCode = char.charCodeAt(0);
+    if(charCode >=65 && charCode <=90){
+      return char.toLowerCase();
+    } else if ((charCode >= 97 && charCode <= 122) || (charCode >= 48 && charCode <= 57)) {
+      return char;
+    } else {
+      return "invalid";
+    }
+  }
+
+  for(var i = 0, j = s.length -1; i <= j;){
+    if(isLetter(s[i]) === "invalid"){
+      i++;
+      continue;
+    };
+    if(isLetter(s[j]) === "invalid"){
+      j--;
+      continue;
+    };
+    console.log(isLetter(s[i]), isLetter(s[j]), i, j);
+    if(isLetter(s[i]) !== isLetter(s[j])) return false;
+    i++;
+    j--;
+  }
+  return true;
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
