5a649883cbbca03f3d8640875249a49253a6c1e7
MadhuBeesetty <madhusudhan9kings.com>
2024-09-04 19:59:38 -0700

    initial commit for weather app

---
+++ weather-app/src/components/Mainpage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <header>
+         weather app main component
+      </header>
+    </div>
+  );
+}
+
+export default MainPage;
