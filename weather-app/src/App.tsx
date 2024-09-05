61c286c188d5ef59d88ce46baaed1db7dcd64d75
MadhuBeesetty <madhusudhan9kings.com>
2024-09-04 20:33:11 -0700

    weather app progress

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainPage from '../src/components/Mainpage';
+import CityInput from '../src/components/CityInput';
 
 const App = () => {
   return (
     <div className="App">
       <MainPage />
+      <CityInput />
       <header className="App-header">
           Learn React
       </header>
