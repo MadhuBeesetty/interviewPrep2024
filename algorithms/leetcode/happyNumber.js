7f7eeb26e5316330ad07ca36c4cefa413e23e4cd
MadhuBeesetty <madhusudhan9kings.com>
2024-09-08 21:34:40 -0700

    happy number

--- algorithms/leetcode/happyNumber.js
+++ algorithms/leetcode/happyNumber.js
@@ -22,25 +22,25 @@
 // Output: false
 
 const isHappy = (num) => {
-let set = new Set();
-  const calculatesSquares = (n) => {
-    let arr = [];
-    let stringConversion = n.toString();
-    arr = [...stringConversion];
-    console.log(arr);
-    let sum = 0;
-    arr.forEach((eachNumber) => sum = sum + eachNumber);
-    if(set.has(sum)){
-      return false;
-    }else{
-      
-      set.add(sum);
-      calculatesSquares(Number(sum));
+  let set = new Set();
+    const calculatesSquares = (n) => {
+      let arr = [];
+      let stringConversion = n.toString();
+      arr = [...stringConversion];
+      console.log(arr);
+      let sum = 0;
+      arr.forEach((eachNumber) => sum = sum + eachNumber);
+      if(set.has(sum)){
+        return false;
+      }else{
+
+        set.add(sum);
+        calculatesSquares(Number(sum));
+      }
     }
-  }
 
-  calculatesSquares(num);
+    calculatesSquares(num);
 
-};
+  };
 
-console.log(isHappy(19));
+  console.log(isHappy(19));
